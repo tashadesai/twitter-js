9a51908df3b6968327d0eb1b4831bb95c293ed41
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-07 11:00:50 -0500

    through step 3 w/o extra credit

--- app.js
+++ app.js
@@ -2,9 +2,23 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.url);
+  next();
+});
+
 app.get('/', (request, response) => {
   response.send('welcome');
 });
+
+app.get('/news', (request, response) => {
+  response.send('THE NEWS');
+});
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('server listening');
